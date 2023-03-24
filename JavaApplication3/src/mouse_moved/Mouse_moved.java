f3fcf8327558734806fae52a47ea81eddffa07ad
Omar-Gtz403 <111006989+Omar-Gtz403@users.noreply.github.com>
2023-03-23 21:01:16 -0600

    aplicacion

---
+++ JavaApplication3/src/mouse_moved/Mouse_moved.java
@@ -0,0 +1,48 @@
+
+package mouse_moved;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class Mouse_moved extends JPanel implements MouseMotionListener{
+
+int x,y;
+String texto="";
+public Mouse_moved(){
+    addMouseMotionListener(this);
+}
+@Override
+public void paint(Graphics g){
+    ImageIcon imagen =new ImageIcon(getClass().getResource("star.png"));
+    g.drawImage(imagen.getImage(), x, y,32,32,null);
+    Font fuente =new Font("ARIAL",Font.BOLD,22);
+    g.setFont(fuente);
+    g.drawString(texto, 20, 400);
+}
+    public static void main(String[] args) {
+JFrame f = new JFrame ("Ejemplo con Mouse_Moved");
+f.getContentPane().add(new Mouse_moved());
+f.setLocationRelativeTo(null);
+f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+f.setSize(500, 500);
+f.setVisible(true);
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        x=(int)e.getPoint().getX();
+        x=(int)e.getPoint().getY();
+        repaint();
+    }
+    
+}
